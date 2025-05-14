1d1580cfde5cd0ec11d45818eec063d021565a64
Simon Gärtner <simon.gartner@positiongreen.com>
2025-05-14 21:32:20 +0200

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/secret-code-journey/" : "/",
+  base: mode === "production" ? "/mystery-hunt/" : "/",
   server: {
     host: "::",
     port: 8080,
